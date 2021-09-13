b784b2f80ed7067a444d99680642faf9fc9bdf49
Sameony <iam2001sam@gmail.com>
2021-09-13 15:34:14 +0530

    feat: added media

--- index.js
+++ index.js
@@ -3,13 +3,30 @@ const stonks = document.querySelector("#stonks");
 const curPrice = document.querySelector("#curPrice");
 const btn_check = document.querySelector(".btn-check");
 const outputHere = document.querySelector(".outputHere");
+const profit_gif = document.querySelector(".profit_gif");
+const Very_high_loss_png = document.querySelector(".Very_high_loss_png");
+const loss_gif = document.querySelector(".loss_gif");
+const loader = document.querySelector(".loader");
 
-btn_check.addEventListener("click", function profit_or_loss() {
+loader.style.display="none";
+Very_high_loss_png.style.display="none";
+loss_gif.style.display="none";
+profit_gif.style.display="none";
+
+
+btn_check.addEventListener("click", function clickEventHandler(){
+
+} );
+
+function profit_or_loss() {
 
     var sellingPrice = curPrice.value * stonks.value;
     var costPrice = init.value * stonks.value;
+    var differ = Math.abs(costPrice-sellingPrice);
+    var percent = parseFloat((differ/costPrice)*100).toFixed(2);
+    
     if ((sellingPrice - costPrice) < 0) {
-        console.log("loss");
+        outputHere.innerText=("loss of "+differ+" and the percentage of loss is "+percent);
     } else
-        console.log("stonks!");
-});
+        outputHere.innerText=("Profit of "+differ+" and the percentage of profit is "+percent);
+}
