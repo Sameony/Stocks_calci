049562cb660e9fc827691c234a9cf983ce54e9e3
Sameony <iam2001sam@gmail.com>
2021-09-13 13:33:13 +0530

    feat: added input methods

---
+++ index.js
@@ -0,0 +1,6 @@
+const init = document.querySelector("#initPrice");
+const stonks = document.querySelector("#stonks");
+const curPrice = document.querySelector("#curPrice");
+const btn_check = document.querySelector(".btn-check");
+const outputHere = document.querySelector(".outputHere");
+
