4e10b7256ba7b44a6ce43f87b73e7138bd865fe6
Sameony <iam2001sam@gmail.com>
2021-09-13 13:45:13 +0530

    feat: costprice n selling price

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const curPrice = document.querySelector("#curPrice");
 const btn_check = document.querySelector(".btn-check");
 const outputHere = document.querySelector(".outputHere");
 
+btn_check.addEventListener("click", function profit_or_loss() {
+
+    var sellingPrice = curPrice.value * stonks.value;
+    var costPrice = init.value * stonks.value;
+    if ((sellingPrice - costPrice) < 0) {
+        console.log("loss");
+    } else
+        console.log("stonks!");
+});
